234be9254b5f1023de3bf44b4bb7b5ec7fb84121
YuliaSabitova <yuliasab2017@mail.com>
2026-02-09 10:41:01 +0200

    add changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { Person } from './types/Person';
 import { peopleFromServer } from './data/people';
 import { Dropdown } from './Components/Autocomplete';
 
-
 export const App: React.FC = () => {
-
   const [selected, setSelected] = useState<Person | null>(null);
-
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
-        <h1 className="title" data-cy="title">
+        <h1 className="title"
+        data-qa="title"
+        data-cy="title">
           {selected
             ? `${selected.name} (${selected.born} - ${selected.died})`
             : 'No selected person'}
         </h1>
 
-        <Dropdown
-        people={peopleFromServer}
-        onSelected={setSelected}
-        />
+        <Dropdown people={peopleFromServer} onSelected={setSelected} />
       </main>
     </div>
   );
--- src/Components/Autocomplete.tsx
+++ src/Components/Autocomplete.tsx
@@ -1,55 +1,59 @@
-import { useEffect, useMemo, useRef, useState } from "react";
-import { Person } from "../types/Person";
-import debounce from "lodash.debounce";
-
+import { useEffect, useMemo, useRef, useState } from 'react';
+import { Person } from '../types/Person';
+import debounce from 'lodash.debounce';
 
 type Props = {
   people: Person[];
   onSelected: (person: Person | null) => void;
   delay?: number;
-}
+};
 export const Dropdown: React.FC<Props> = ({
   people,
   onSelected,
   delay = 300,
 }) => {
-    const [query, setQuery] = useState('');
-    const [appliedQuery, setAppliedQuery] = useState('');
-    const [isOpen, setIsOpen] = useState(false);
-    const inputRef = useRef<HTMLInputElement>(null);
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-    const applyQuery = useMemo(
+  const applyQuery = useMemo(
     () =>
       debounce((value: string) => {
         setAppliedQuery(value);
       }, delay),
     [delay],
   );
+
   useEffect(() => {
-      return () => {
-        applyQuery.cancel();
-      };
-    }, [applyQuery]);
+    return () => {
+      applyQuery.cancel();
+    };
+  }, [applyQuery]);
 
-    useEffect(() => {
-      inputRef.current?.focus();
-    }, []);
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
 
-    const suggestion = useMemo(() => {
-      if (isOpen && query.trim() === '') {
-        return people;
-      }
+  const filteredPeople = useMemo(() => {
+  if (!appliedQuery.trim()) {
+    return [];
+  }
 
-      if (!appliedQuery.trim()) {
-        return [];
-      }
+  return people.filter(person =>
+    person.name
+      .toLowerCase()
+      .includes(appliedQuery.toLowerCase()),
+  );
+}, [appliedQuery, people]);
 
-      return people.filter(person =>
-        person.name.toLowerCase().includes(appliedQuery.toLowerCase()),
-      );
-    }, [appliedQuery, isOpen]);
+  const suggestions =
+  isOpen && query.trim() === ''
+    ? people
+    : filteredPeople;
 
-const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newQuery = event.target.value;
 
     if (newQuery === query) {
@@ -70,48 +74,53 @@ const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 
   return (
     <div className={`dropdown ${isOpen ? 'is-active' : ''}`}>
-          <div className="dropdown-trigger">
-            <input
-              ref={inputRef}
-              type="text"
-              className="input"
-              placeholder="Enter a part of the name"
-              value={query}
-              data-cy="search-input"
-              onChange={handleQueryChange}
-              onFocus={() => setIsOpen(true)}
-            />
-          </div>
+      <div className="dropdown-trigger">
+        <input
+          ref={inputRef}
+          type="text"
+          className="input"
+          placeholder="Enter a part of the name"
+          value={query}
+          data-qa="search-input"
+          data-cy="search-input"
+          onChange={handleQueryChange}
+          onFocus={() => setIsOpen(true)}
+        />
+      </div>
 
-          <div className="dropdown-menu" role="menu">
-            <div className="dropdown-content" data-cy="suggestions-list">
-              {suggestion.map(person => (
-                <div
-                  key={person.slug}
-                  className="dropdown-item"
-                  data-cy="suggestion-item"
-                  onClick={() => handleSelect(person)}
-                >
-                  <p className="has-text-link">{person.name}</p>
-                </div>
-              ))}
+      <div className="dropdown-menu" role="menu">
+        <div className="dropdown-content"
+        data-qa ="suggestions-list"
+        data-cy ="suggestions-list">
+          {suggestions.map(person => (
+            <div
+              key={person.slug}
+              className="dropdown-item"
+              data-qa="suggestion-item"
+              data-cy="suggestion-item"
+              onClick={() => handleSelect(person)}
+            >
+              <p className="has-text-link">{person.name}</p>
             </div>
-          </div>
+          ))}
+        </div>
+      </div>
 
-        {isOpen && query !== '' && suggestion.length === 0 && (
-          <div
-            className="
+      {isOpen && appliedQuery.trim() !== '' && filteredPeople.length === 0 && (
+        <div
+          className="
             notification
             is-danger
             is-light
             mt-3
             is-align-self-flex-start"
-            role="alert"
-            data-cy="no-suggestions-message"
-          >
-            <p className="has-text-danger">No matching suggestions</p>
-          </div>
-       )}
+          role="alert"
+          data-qa="no-suggestions-message"
+          data-cy="no-suggestions-message"
+        >
+          <p className="has-text-danger">No matching suggestions</p>
+        </div>
+      )}
     </div>
   );
 };
